76ce4998d1dd4ae995b665a8a58de3fd8e03619f
David Gold <david.gold.a@gmail.com>
2017-10-09 18:27:21 -0700

    Bug fix

--- src/trial.r
+++ src/trial.r
@@ -39,7 +39,7 @@ trial <- function(tau=1.1) {
 
   # Estimation for exogenous, partially linear components
   X12 <- cbind(X1,X2)
-  gamma.hat <- solve(t(X12)%*%X12) %*% t(X12) %*% (y-W%*%beta_Lasso_Xhat[3:length(beta_Lasso_Xhat)])
+  gamma.hat <- solve(t(X12)%*%X12) %*% t(X12) %*% (y-W%*%beta_Lasso_D.hat[3:length(beta_Lasso_D.hat)])
   SE.gamma.hat <- solve(t(X12)%*%X12) * sdu.hat^2
 
   # To record/compute estimate of Theta_jj (and Theta_jj)
